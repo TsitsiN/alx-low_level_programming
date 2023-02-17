4f8e27c45bba932aa339dd0ebb2b67bf00c31c0f
TsitsiN <tsamuriwo88@gmail.com>
2023-02-17 15:34:53 -0800

    Program checks positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,9 +5,16 @@
 #include <stdio.h>
 
 /* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
 
-/* betty style doc for function main goes there */
 int main(void)
+/*This is what main is doing */
 {
 int n;
 
