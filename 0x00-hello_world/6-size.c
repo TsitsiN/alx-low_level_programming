8ab3e0ad5771b74e7a4bf6f29641e5f881a2bb3f
TsitsiN <tsamuriwo88@gmail.com>
2023-02-16 16:17:51 -0800

    Print data size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,16 @@
 */
 int main(void)
 {
-printf("Size of a char: %ts",sizeof(charType);
-printf("Size of an int: %ts",sizeof(intType);
-printf("Size of a long int: %ts",sizeof(longType);
-printf("Size of a long long int: %ts",sizeof(longType);
-printf("Size of a float: %ts",sizeof(floatType);
+char charType;
+int intType;
+long longType;
+float floatType;
+
+printf( "Size of a char: %ts ",sizeof(charType);
+printf( "Size of an int: %ts ",sizeof(intType);
+printf( "Size of a long int: %ts ",sizeof(longType);
+printf( "Size of a long long int: %ts ",sizeof(longType);
+printf( "Size of a float: %ts ",sizeof(floatType);
   
 return (0);
 }
