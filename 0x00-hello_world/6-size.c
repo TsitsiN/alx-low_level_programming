2702b60ea4dcc7f7a9ccfb10accb5c20fbb9b8eb
TsitsiN <tsamuriwo88@gmail.com>
2023-02-16 16:53:45 -0800

    Print data size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,5 @@ printf("Size of an int: %zu bytes(s)\n", sizeof(int));
 printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(double));
 printf("Size of a float: %zu byte(s)", sizeof(float));
- 
 return (0);
 }
