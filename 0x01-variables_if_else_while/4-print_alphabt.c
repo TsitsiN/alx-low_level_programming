105f0d1c44db1bee2f48667022ec4bb56e958f3e
TsitsiN <tsamuriwo88@gmail.com>
2023-02-17 17:49:43 -0800

    Print lowercase letters except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+/* Print lowercase a to z except q and e */
+{
+int ch;
+
+ch = 97;
+
+for ch != 101 or ch != 113;
+
+do {
+putchar(ch);
+ch++;
+} while (ch < 123);
+putchar(10);
+return (0);
+}
