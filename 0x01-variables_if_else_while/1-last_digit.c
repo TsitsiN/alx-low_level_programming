e386be48c58340cc5e9526bc4c8db8b7fd9abb8b
TsitsiN <tsamuriwo88@gmail.com>
2023-02-17 16:11:17 -0800

    Program prints last digit of random number with conditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,33 @@
 #include <stdlib.h>
+
 #include <time.h>
+
+#include <stdio.h>
+
 /* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
 
-/* betty style doc for function main goes there */
 int main(void)
+/*This is what main is doing */
 {
-	int n;
+int n, lastDigit;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* My Task 1 code */
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+  /* My task 0 Code */
+
+lastDigit = n % 10;
+if (lastDigit > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+else if (n == 0)
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+else
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+return (0);
 }
