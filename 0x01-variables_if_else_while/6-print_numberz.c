4e27f3612eddeaddcd52abc34361e04d9cbebd7c
TsitsiN <tsamuriwo88@gmail.com>
2023-02-17 18:49:36 -0800

    Code prints numbers using putchar()

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+/* Prints numbers 0 to 9*/
+{
+char num;
+
+for (num = 0 ; num < 10 ; num++)
+putchar(num);
+putchar(10);
+return (0);
+}
