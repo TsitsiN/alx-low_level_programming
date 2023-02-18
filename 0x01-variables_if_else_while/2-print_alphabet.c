55d5a79a5be2392ba0cb5455178a2442f4511772
TsitsiN <tsamuriwo88@gmail.com>
2023-02-17 16:52:15 -0800

    Print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+/* Print lowercase a to z */
+{
+ 
+
+  for (  ch = 97
+    do
+      { putchar(ch);
+	ch++
+      } while ch < 123
+return (0);
+}
