19365f7c062c4069c37a6cbe9c7b8a4840253d96
TsitsiN <tsamuriwo88@gmail.com>
2023-02-19 07:56:58 -0800

    Print letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,17 +14,16 @@
 */
 
 int main(void)
-/* Print lowercase a to z except q and e */
+/* Print lowercase a to z */
 {
 int ch;
 
 ch = 97;
-for ( ; ; )
+ for ( ; ch != 101 || ch != 113 ; );
 do {
 putchar(ch);
 ch++;
-} while
-(ch < 123);
+} while (ch < 123);
 putchar(10);
 return (0);
 }
