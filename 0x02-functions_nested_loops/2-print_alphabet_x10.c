4f68d1297310a2e10a00dae09fa20d095a9ecc96
TsitsiN <tsamuriwo88@gmail.com>
2023-02-21 16:06:30 -0800

    prints alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,9 +14,10 @@ void print_alphabet_x10(void)
 int i;
 char ch;
 
-for(i=1;i<11;i++){
+for(i = 1 ;i < 11 ;i++)
+{
 
-for (ch=97; ch < 123; ch++)
+for (ch = 97; ch < 123; ch++)
 {
 _putchar(ch);
 _putchar('\n');
