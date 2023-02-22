ca2b882631ac72369a9e15259a75b0b40425c511
TsitsiN <tsamuriwo88@gmail.com>
2023-02-22 02:07:29 -0800

    prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/* more headers goes there */
+/**
+ * _int print_sign- print function that prints the sign of a number - Entry point
+ *
+ *Description: 'print the sign of a number'
+ *
+ *
+ * Return: 1 and prints + if n is greater than 0 else return 0 and print 0 if n=0  otherwise print -1 if n is less than zero and reurn -1 (Success)
+*/
+
+int print_sign(int n)
+{
+if ('n' > 0)
+print '+'
+return (1);
+else if ('n' = 0)
+return (0);
+else ('n' < 0)
+print (-)
+return (-1);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c~
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/* more headers goes there */
+/**
+ * _isalpha - print function that checks for alphabetic character - Entry point
+ *
+ * @c: c is an ASCII integer
+ *Description: 'print parameters'
+ *
+ *
+ * Return: 1 if it is a lowercase, uppercase and 0 otherwise (Success)
+*/
+
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+return (1);
+else
+return (0);
+}
