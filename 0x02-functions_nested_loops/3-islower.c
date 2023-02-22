baa918d947f772df6356cb74c0a9e8c429f5b6c5
TsitsiN <tsamuriwo88@gmail.com>
2023-02-22 00:55:29 -0800

    print function for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,9 @@
 /**
  * _islower - print function that checks for lowercase characters - Entry point
  *
- * Description: 'print parameters' 
+ * Description: 'print parameters'
+ *
+ * c - is a lowercase character
  *
  * Return: 1 if it is a lower case and 0 otherwise (Success)
 */
