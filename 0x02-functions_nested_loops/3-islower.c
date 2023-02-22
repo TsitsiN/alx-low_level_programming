aa18daedcab5069a5f19eca102da7cb11a4d2f32
TsitsiN <tsamuriwo88@gmail.com>
2023-02-21 23:53:54 -0800

    print function for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/* more headers goes there */
+/**
+ * print function that checks for lowercase characters - Entry point
+ *
+ * Description: 'print lowercase characters'
+ *
+ * Return: 1 if it is a lower case and 0 otherwise (Success)
+*/
+
+int _islower(int c)
+{
+if (c>= 97 && c<= 122)
+return (1);
+else
+return (0);
+{
