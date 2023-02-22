26d203db6eaa1701b882ed4e0b9966547c1a3d22
TsitsiN <tsamuriwo88@gmail.com>
2023-02-22 02:15:19 -0800

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,12 +12,12 @@
 
 int print_sign(int n)
 {
-if ('n' > 0)
-print '+'
+if (n > 0)
+printf("+");
 return (1);
-else if ('n' = 0)
+else if (n == 0)
 return (0);
-else ('n' < 0)
-print (-)
+else (n < 0)
+printf("-");
 return (-1);
 }
