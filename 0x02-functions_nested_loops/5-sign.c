5a0ecb6882e53ed59c8279d9c7fcca08467a4393
TsitsiN <tsamuriwo88@gmail.com>
2023-02-22 13:07:12 -0800

    prints the sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,27 +2,25 @@
 
 /* more headers goes there */
 /**
- * Engtry Point - print_sign- print function that prints the sign of a number.
+ * Entry Point - print_sign- print function that prints the sign of a number.
+ * 
+ * @n: the input number is a integer
+ *Description: 'determines if the input isgreater, equal or less than zero
  *
- *Description: 'print the sign of a number'
  *
- *
- * Return: 1 and prints + if n is greater than 0 else return 0 and print 0 if n=0  otherwise print -1 if n is less than zero and reurn -1 (Success)
+ * Return: 1 and prints + if n is greater than 0 else return 0 and print 0 if n=0  otherwise print -1 if n is less than zero and return -1 (Success)
 */
 
 int print_sign(int n)
 {
- 
 if (n > 0)
-  /*   _putchar('1'); */
-/*  *_putchar(',');*/
- _putchar('+');
- return (1);
-
- if (n <0)
-   _putchar('-');
- return (-1);
-  if (n == 0)
-       _putchar('0');
+_putchar(43);
+return (1);
+else if (n < 0)
+_putchar(45);
+return (-1);
+else (n == 0)
+_putchar(48);
 return (0);
+_putchar('\n')
 }
