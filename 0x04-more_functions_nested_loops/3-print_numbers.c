cbf64fd7077017d0515ae183f6e63f396fc06db4
TsitsiN <tsamuriwo88@gmail.com>
2023-02-23 15:23:06 -0800

    Function to print integers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,19 +6,18 @@
  *
  * Description:  'Checks digit input.'
  *
- *@c: the input integer
  *
  * Return: 0 Success.
  *
  */
-int print_numbers(int c)
+void print_numbers(void)
 {
-c = 48;  
+c = 48;
 for ( ; c <= 57; c++)
 {
 _putchar(c);
 _putchar(10);
 /* 10 is the ASCII code for newline */
 }
-return (0);
+
 }
