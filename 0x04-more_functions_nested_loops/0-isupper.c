fb7c457dd12dce5b62f261c8cac9e2201605aabd
TsitsiN <tsamuriwo88@gmail.com>
2023-02-23 14:08:59 -0800

    Base files and task 0 commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/* more headers goes there */
+/**
+ * int _isupper - case check function
+ *
+ * Description:  'Checks fior uppercase character.'
+ *
+ *@c: the input character
+ *
+ * Return: 1 if c is upper case , otherwise return 0.
+ *
+ */
+int print_sign(int c)
+{
+if (c >= 65 || c<= 90)
+return (1); }
+else
+return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains various functions amd loops
