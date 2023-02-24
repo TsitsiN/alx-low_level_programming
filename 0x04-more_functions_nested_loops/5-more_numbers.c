8c7302b8e7ab8518fae41be019076e9fa27f8034
TsitsiN <tsamuriwo88@gmail.com>
2023-02-24 14:09:16 -0800

    prints the numbers and exclude some

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /* more headers goes there */
 /**
- * print_most_numbers - function prints numbers
+ * print_numbers - digit check function
  *
  * Description:  'Checks digit input.'
  *
@@ -10,17 +10,16 @@
  * Return: 0 Success.
  *
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
 int c;
 
-for (c = 48; c < 58; c++)
-{
-if (c != 50)
+for (c = 48; c <= 57; c++)
 {
 _putchar(c);
 }
-}
-}
-_putchar('\n');
+_putchar(10);
+/* 10 is the ASCII code for newline */
+
+
 }
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -3,3 +3,4 @@ Task 0 - - Uppercase checking function.
 Task 1 - int _isdigit(int c) - Digit checking function.
 Task 2 - - int mul(int a, int b) - Function multiples two integers.
 Task 4 -  - void print_most_numbers(void) - Function to prints numbers except 2 and 4
+Task 5  - - void more_numbers(void); - function to print numbers 10 times
