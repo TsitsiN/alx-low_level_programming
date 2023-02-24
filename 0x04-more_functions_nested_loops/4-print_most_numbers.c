49c2a1c5596ac7f7cef11e3ea17720120a5c9753
TsitsiN <tsamuriwo88@gmail.com>
2023-02-23 16:00:52 -0800

    Function prints most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,10 +14,8 @@ void print_most_numbers(void)
 {
 int c;
 
-for (c = 48; c <= 57; c++)
+for (c = 48; c <= 57 && (c != 50 || c != 52); c++)
 {
-while
-(c != 50 || c != 52);
 _putchar(c);
 }
 _putchar(10);
