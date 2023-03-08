42fbe05d2fb0500be5d4d2d84e8a32892ba30eac
TsitsiN <tsamuriwo88@gmail.com>
2023-03-08 15:55:25 -0800

    prints length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen_recursion - returns the length of a string
+* @s: the string to be measured
+*
+* Return: the length of string
+*/
+int _strlen_recursion(char *s)
+{
+int len = 0;
+
+if (*s)
+{
+len++;
+len += _strlen_recursion(s + 1);
+}
+}
