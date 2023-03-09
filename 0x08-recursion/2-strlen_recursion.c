ea09b47e41a2a45cb8036acbec40129bc3e0144e
TsitsiN <tsamuriwo88@gmail.com>
2023-03-08 16:19:41 -0800

    prints length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+11;rgb:0000/0000/0000#include <stdio.h>
 
 /**
 * _strlen_recursion - returns the length of a string
@@ -16,4 +16,5 @@ if (*s)
 len++;
 len += _strlen_recursion(s + 1);
 }
+return (0);
 }
