cbcf2155be2fc0a090856673f7d2aca344417a0a
TsitsiN <tsamuriwo88@gmail.com>
2023-03-10 16:29:40 -0800

    static libraries

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,24 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
-/* more headers goes there */
 /**
- * main - Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Description: 'print letterrs for putchar'
- *
- * Return: Always 0 (Success)
-*/
-
-int main(void)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	return (write(1, &c, 1));
 }
